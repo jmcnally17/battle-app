8da5b878079591ac6f1c5c90031c3cf7a8c2e888
Joe McNally <joe.t.mcnally@hotmail.com>
2022-04-07 16:43:25 +0100

    Created feature test for the Attack button and a confirmation message that player 1 attacked player 2.

---
+++ week-3/battel_app/spec/features/attack_spec.rb
@@ -0,0 +1,9 @@
+require_relative '../../app'
+
+feature 'attack' do
+  scenario 'attacks player 2' do
+    sign_in_and_play
+    click_button 'Attack'
+    expect(page).to have_content "Ruzeka attacks Joe!"
+  end
+end
