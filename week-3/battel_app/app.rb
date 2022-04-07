bca8b813fb6ed12ebb20d579c94217a7bc525e2e
Ruzeka Uddin <ruzekauddin@hotmail.co.uk>
2022-04-07 15:50:49 +0100

    Post/redirect/Get pattern

--- week-3/battel_app/app.rb
+++ week-3/battel_app/app.rb
@@ -3,6 +3,7 @@ require 'sinatra/reloader'
 
 
 class Battle < Sinatra::Base
+  enable :sessions
   configure :development do
     register Sinatra::Reloader
   end
@@ -12,8 +13,14 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
+    session['player_1_name'] = params[:player_1_name]
+    session['player_2_name'] = params[:player_2_name]
+    redirect to '/play'
+  end
+
+  get '/play' do
+    @player_1_name = session['player_1_name'] 
+    @player_2_name = session['player_2_name']
     erb :play
   end
   
