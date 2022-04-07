54607e94f01105bea0006c64bc4605894576f2f2
Ruzeka Uddin <ruzekauddin@hotmail.co.uk>
2022-04-07 16:11:11 +0100

    Test Helper

--- week-3/battel_app/spec/features/enter_names_spec.rb
+++ week-3/battel_app/spec/features/enter_names_spec.rb
@@ -2,10 +2,7 @@ require_relative '../../app.rb'
 
 feature "enter names" do
   scenario "submitting names" do
-    visit('/')
-    fill_in :player_1_name, with: 'Ruzeka'
-    fill_in :player_2_name, with: 'Joe'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content "Ruzeka vs Joe"
   end
 end
